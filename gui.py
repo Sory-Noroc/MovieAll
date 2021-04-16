15c5e0d08e8cd94a4feca56758e3511ece09d89f
Sory <sorinnoroc1@gmail.com>
2021-04-16 20:18:00 +0300

    Button

--- gui.py
+++ gui.py
@@ -5,19 +5,22 @@ import sys
 
 
 class Ui:
-	''' The GUI that holds all the widgets that the User will interact with '''
+	''' The Vertical Layed GUI that holds all the widgets that the User will interact with '''
 
-	def __init__(self):
-		''' Initializes the GUI and '''
+	def __init__(self, width=800, height=600):
+		''' Initializes the GUI and it's properties '''
 
 		self.app = QtWidgets.QApplication(sys.argv)
 		self.mw = QtWidgets.QMainWindow()
-		self.mw.resize(800, 600)
+		self.mw.resize(width, height)
 
-		self.centralwidget = QtWidgets.QWidget(self.mw)
+		self.widgets = QtWidgets
+
+		self.centralwidget = self.widgets.QWidget(self.mw)
 		self.mw.setCentralWidget(self.centralwidget)
 
-		self.centralframe = QtWidgets.QVBoxLayout()
+		self.centralframe = self.widgets.QVBoxLayout()
+		self.centralwidget.setLayout(self.centralframe)
 
 	def place_frame(self, frame):
 		''' 
@@ -27,6 +30,13 @@ class Ui:
 		widget = QtWidgets.QWidget()
 		widget.setLayout(frame)
 		self.centralframe.addWidget(widget)
+
+	def button(self, connect_func):
+		''' Creates and places a button, also connects it to a function(clicked)'''
+		button = self.widgets.QPushButton(self.centralwidget)
+		button.clicked.connect(connect_func)
+
+		return button
 
 	def run(self):
 		self.mw.show()
